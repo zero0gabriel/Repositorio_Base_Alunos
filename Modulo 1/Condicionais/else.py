b418d160f106378479b147793e50eb12456a7228
Gabriel <gs7313859@gmail.com>
2026-05-12 08:37:30 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/else.py
@@ -0,0 +1,12 @@
+nome = input("Qual e o seu nome? ")
+idade = int(input("qual e a sua idade? "))
+
+if idade >= 18:
+  
+    print("Maior idade")
+    print("executa 01")
+    print("executa 02")
+else:
+       print ("menor idade")
+       print("intrucao 01")
+       print("instrucao 02")
